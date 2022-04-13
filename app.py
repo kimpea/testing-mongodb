6ffd6448c9c0d84d58d903633514a63d6dfac235
Kim <kim.pearton@live.co.uk>
2022-04-13 13:25:08 +0100

    Remove line 16

--- app.py
+++ app.py
@@ -13,9 +13,8 @@ mongo = PyMongo(app)
 
 @app.route('/index')
 def index():
-    articles = mongo.db.articles.find()
     return render_template("index.html",
-                            articles=articles)
+                            articles=mongo.db.articles.find())
 
 
 
