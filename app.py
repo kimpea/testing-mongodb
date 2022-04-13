57121307e511c375989aa6097c99b36e6db3cd5a
Kim <kim.pearton@live.co.uk>
2022-04-13 16:48:09 +0100

    Testing

--- app.py
+++ app.py
@@ -14,9 +14,8 @@ db = mongo.db
 @app.route('/')
 def index():
     articles = db.articles
-    article_name = articles.article_name
+    articles = list(articles.find())
     return render_template("index.html",
-                            article_name=article_name,
                             articles=articles)
 
 @app.route('/get_article')
