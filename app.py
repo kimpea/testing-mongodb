412cb65d21559d7ba5a2256b17088395ce69584a
Kim <kim.pearton@live.co.uk>
2022-04-13 11:39:05 +0100

    Testing

--- app.py
+++ app.py
@@ -1,37 +1,20 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for, session  
 from flask_pymongo import PyMongo
-from pymongo.server_api import ServerApi
 from bson.objectid import ObjectId
 import math
 
 app = Flask(__name__)
-
 app.config["MONGO_DBNAME"] = 'article-manager'
-app.config["MONGO_URI"] = "mongodb+srv://kimpearton:<ehCCRM3DC6yY8Du>@cluster0.xuetz.mongodb.net/article-manager?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "mongodb+srv://<admin>:<JPgRRe7K0OLr3bft>@<articlescluster>.mongodb.net/article-manager"
 mongo = PyMongo(app)
 
 @app.route('/')
 
 @app.route('/index')
 def index():
-    return render_template ("index.html")
-
-
-@app.route('/getarticle', methods=['GET'])
-def get_article(article_name, article_content, article_author):
-    the_article = mongo.db.articles.find({
-        "article_name": article_name,
-        "article_content": article_content,
-        "article_author": article_author
-        })
-    return render_template("getarticle.html",
-                            article=the_article,
-                            article_name=mongo.db.articles.find({"article_name": article_name}),
-                            article_content=article_content,
-                            article_author=article_author
-                            )
-
+    return render_template ("index.html",
+    article=mongo.db.articles.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
