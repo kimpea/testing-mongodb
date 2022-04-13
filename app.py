12fc30c55690bf2f85adbd4cda65df23697a000b
Kim <kim.pearton@live.co.uk>
2022-04-13 15:05:59 +0100

    Edit def index

--- app.py
+++ app.py
@@ -13,8 +13,9 @@ mongo = PyMongo(app)
 
 @app.route('/index')
 def index():
+    articles = mongo.db.articles.find()
     return render_template("index.html",
-                            articles=mongo.db.articles.find())
+                            articles=articles
 
 @app.route('/get_article/<article_id>')
 def get_article(article_id):
