dc566a2e16ff78d1a3cb1e54e4eb1a86dc568146
Kim <kim.pearton@live.co.uk>
2022-04-13 17:17:41 +0100

    Testing

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from bson.objectid import ObjectId
 import math
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = 'article-manager'
 app.config["MONGO_URI"] = 'mongodb+srv://admin:pWSlyvkLUOph5go0@articlescluster.e9swl.mongodb.net/article-manager'
 mongo = PyMongo(app)
 db = mongo.db
@@ -17,7 +16,7 @@ def index():
     article_data = list(articles.find({}))
     return render_template("index.html",
                             articles=article_data)
-                            
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
     port=int(os.environ.get('PORT', 5000)),
