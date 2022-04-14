cf89c1f8012dae8b42ad899730bc521eca12920b
Kim <kim.pearton@live.co.uk>
2022-04-14 09:33:44 +0100

    Test

--- app.py
+++ app.py
@@ -1,29 +1,20 @@
-from flask import Flask, render_template, redirect, request, url_for, session
-from flask_pymongo import PyMongo
+from flask import Flask, render_template, url_for
+from flask_pymongo import pymongo
 from bson.objectid import ObjectId
-import math
 import os
 
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = 'article-manager'
-app.config["MONGO_URI"] = 'mongodb+srv://admin:pWSlyvkLUOph5go0@articlescluster.e9swl.mongodb.net/article-manager'
-mongo = PyMongo(app)
-db = mongo.db
 
-@app.route('/')
+CONNECTION_STRING = 'mongodb+srv://admin:pWSlyvkLUOph5go0@articlescluster.e9swl.mongodb.net/article-manager'
+client = pymongo.MongoClient('CONNECTION_STRING')
+db = client.get_database('article-manager')
+articles_collection = pymongo.collection.Collection(db, 'articles')
 
-@app.route('/index')
-def index():
-    return render_template("index.html",
-                            articles=db.articles.find())
-
-
-@app.route('/get_article/<article_id>')
-def get_article(article_id):
-    the_article = db.articles.find_one({"_id": ObjectId(article_id)})
-    return render_template("getarticle.html",
-                            article=the_article)
+@app.route('/test')
+def test():
+    db.db.collection.insert_one({"article_name": "Test Article"})
+    return "Connected to the data base!"
 
 
 
